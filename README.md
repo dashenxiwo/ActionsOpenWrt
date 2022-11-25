b9e8b64cffe5df5f57162b8c385ad28f492b2601
dev <dev@demo.com>
2022-11-25 18:27:04 +0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+Build OpenWrt using GitHub Actions
+
+这是OpenWRT专用自动编译脚本
+
+[云配置生成](https://hackyes.github.io/openwrt-menuconfig/index.html)
---
+++ diy-part1.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+cd package
+mkdir openwrt-packages
+cd openwrt-packages
+git clone https://github.com/fw876/helloworld.git
+cd helloworld
+mv * ../
+cd ../
+ls
+# cd package/lean  
+# rm -rf luci-theme-argon  
+# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  
+# make menuconfig 
+# make -j1 V=s  
+#echo '修改时区'
+#sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+#echo '取消bootstrap主题'
+#sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
